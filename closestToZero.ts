c050fc8d692bd1c3a12d3b562705e15bda2a8a32
So00 <atourner@student.42.fr>
2020-02-13 02:00:57 +0100

    feature/ légère opti du -0 au 0

--- closestToZero.ts
+++ closestToZero.ts
@@ -7,15 +7,17 @@ export default function closestToZero(userInput : Array<number>): number {
 
       // In this case, val is closer
       // Here I use Math.abs to be sure that it's positive
+      // I return 0 if needed because otherwise if val === -0
+      // it return -0 instead of 0
+      if (substraction > 0)
+        return val === 0 ? 0 : val;
+
+      // We return the positive integer if the result is 0
       /**
        * NOTE: Delete Math.abs and check with that args
        * -0 0
        * Surprise :)
        */
-      if (substraction > 0)
-        return val === 0 ? Math.abs(val) : val;
-
-      // We return the positive integer if the result is 0
       if (substraction === 0)
         return acc >= 0 ? Math.abs(acc) : val;
 
